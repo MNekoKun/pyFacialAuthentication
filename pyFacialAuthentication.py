8ad6ccd0a589142a3ce30752a30a4cd1f4cf4eac
Gabriele <sanfilippo.gabriele05@gmail.com>
2023-03-31 19:42:20 +0200

    Bug fixes

--- pyFacialAuthentication.py
+++ pyFacialAuthentication.py
@@ -1,39 +1,75 @@
 from colors import colors
 import sys
+import os
 import cv2
 import face_recognition
 
 def check_face():
     print("Getting password image... ", end="")
     try:
-        image = face_recognition.load_image_file("img/password.jpg")
+        image = face_recognition.load_image_file("password.jpg")
         print(colors.OK + colors.BOLD + "OK" + colors.DEFAULT)
     except FileNotFoundError:
         print(colors.FAIL + colors.BOLD + "\nNo image found" + colors.DEFAULT)
         sys.exit(1)
-    #get the face encoding
-    face_encoding = face_recognition.face_encodings(image)[0]
-    #take a photo from the webcam and save it as img/webcam.jpg
-    webcam = cv2.VideoCapture(0)
-    return_value, image = webcam.read()
-    cv2.imwrite("img/webcam.jpg", image)
-    del(webcam)
-    #load the webcam image
-    webcam_image = face_recognition.load_image_file("img/webcam.jpg")
-    #get the webcam face encoding
+    
+    password_faces = face_recognition.face_encodings(image)
+    if len(password_faces) == 0:
+        print(colors.FAIL + colors.BOLD + "No faces found" + colors.DEFAULT)
+        sys.exit(1)
+    elif len(password_faces) > 1:
+        print(colors.WARNING + colors.BOLD + "WARNING: " + colors.DEFAULT + colors.WARNING + "More than one face found" + colors.DEFAULT)
+        password_face_index = int(input("Which face do you want to use?\n>"))
+        password_face_index -= 1
+    else:
+        password_face_index = 0
+    password_face_encoding = password_faces[password_face_index]
+    
+    print("Getting webcam image... ", end="")
     try:
-        webcam_face_encoding = face_recognition.face_encodings(webcam_image)[0]
-    except IndexError:
-        print("No face found")
-        return
-    #compare the two faces
-    results = face_recognition.compare_faces([face_encoding], webcam_face_encoding)
-    #print the results
-    print(results)
+        webcam = cv2.VideoCapture(0)
+        return_value, image = webcam.read()
+    except:
+        print(colors.FAIL + colors.BOLD + "Something went wrong during webcam capture" + colors.DEFAULT)
+        sys.exit(1)
+    cv2.imwrite("webcam.jpg", image)
+    del(webcam)
+    webcam_image = face_recognition.load_image_file("webcam.jpg")
+    print(colors.OK + colors.BOLD + "OK" + colors.DEFAULT)
+
+    webcam_faces = face_recognition.face_encodings(webcam_image)
+    if len(webcam_faces) == 0:
+        print(colors.FAIL + colors.BOLD + "No faces found" + colors.DEFAULT)
+        sys.exit(1)
+    elif len(webcam_faces) > 1:
+        print(colors.WARNING + colors.BOLD + "WARNING: " + colors.DEFAULT + colors.WARNING + "More than one face found" + colors.DEFAULT)
+        webcam_face_index = int(input("Which face do you want to use? >"))
+        webcam_face_index -= 1
+        if 0>webcam_face_index or webcam_face_index>=len(webcam_faces):
+            print(colors.FAIL + colors.BOLD + "Invalid index" + colors.DEFAULT)
+            sys.exit(1)
+    else:
+        webcam_face_index = 0
+    
+    logImage = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-l" or sys.argv[1] == "--log":
+            logImage = True
+    if not logImage:
+        os.remove("webcam.jpg")
+    
+    webcam_face_encoding = webcam_faces[webcam_face_index]
+    results = face_recognition.compare_faces([password_face_encoding], webcam_face_encoding)
+    return results[0]
 
 def main():
     print(colors.DONE + colors.BOLD + colors.UNDERLINE + "pyFacialAuthentication\n" + colors.DEFAULT)
     authorized = check_face()
+    print(colors.HEADER + colors.BOLD + "Result: " + colors.DEFAULT, end="")
+    if authorized:
+        print(colors.DONE + colors.BOLD + "Authorized" + colors.DEFAULT)
+    else:
+        print(colors.FAIL + colors.BOLD + "Unauthorized" + colors.DEFAULT)
 
 if __name__ == "__main__":
     main()
